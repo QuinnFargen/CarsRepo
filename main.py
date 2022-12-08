dcea47a8fb8b3901d5def02cbcfca5540e265818
Quinn Fargen <quinnfargen@gmail.com>
2022-12-07 23:41:33 -0600

    url builder done

---
+++ main.py
@@ -0,0 +1,10 @@
+
+from CarsURL import carsDotCom_Multi, carsDotCom_Single
+
+
+
+
+
+
+carsDotCom_Multi(page=5,pgsize=50,make='toyota',model='toyota-camry',trim='toyota-camry-se',yrmin='2018',yrmax='2018',mile='',type='used',sort='list_price',maxdist='all',zip='57193')
+carsDotCom_Single('6313112d-5f5e-4b8e-b751-57bfcd331f96')
