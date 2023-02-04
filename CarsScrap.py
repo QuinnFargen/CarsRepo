878dd6eeace574a7075ad61160d4f3123b35dc04
Quinn Fargen <quinnfargen@gmail.com>
2023-02-04 09:53:29 -0600

    Scrap Header list rand

--- CarsScrap.py
+++ CarsScrap.py
@@ -1,6 +1,7 @@
 
 import requests
 import config
+import random 
 from bs4 import BeautifulSoup
 from scraper_api import ScraperAPIClient
 
@@ -10,16 +11,89 @@ def get_soup_safe(carURL):
     result = client.get(url = carURL).text
     return BeautifulSoup(result, 'html.parser')
 
-def get_soup_spicy(carURL):
-    header = {"User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.4040.128"}
-    r = requests.get(carURL) #, headers=header, timeout=5)
+def get_soup_spicy(carURL,refURL):
+    header = random.choice(headers_list)
+    header["Referer"] = refURL
+    r = requests.get(carURL, headers=header)
     return BeautifulSoup(r.text, 'html.parser')
 
-
-
-def get_soup(carURL, useCase):
+def get_soup(carURL,useCase,refURL='https://www.cars.com/'):
     if useCase == 'IDs':
-        return get_soup_spicy(carURL)
+        return get_soup_spicy(carURL,refURL)
     elif useCase == 'Car':
         return get_soup_safe(carURL)
-        
+
+
+headers_list = [
+    # Firefox 77 Mac
+     {
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10.15; rv:77.0) Gecko/20100101 Firefox/77.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        # "Referer": "https://www.google.com/",
+        "DNT": "1",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1"
+    },
+    # Firefox 77 Windows
+    {
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:77.0) Gecko/20100101 Firefox/77.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate, br",
+        # "Referer": "https://www.google.com/",
+        "DNT": "1",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1"
+    },
+    # Chrome 83 Mac
+    {
+        "Connection": "keep-alive",
+        "DNT": "1",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Sec-Fetch-Site": "none",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Dest": "document",
+        # "Referer": "https://www.google.com/",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-GB,en-US;q=0.9,en;q=0.8"
+    },
+    # Chrome 83 Windows 
+    {
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.97 Safari/537.36",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+        "Sec-Fetch-Site": "same-origin",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-User": "?1",
+        "Sec-Fetch-Dest": "document",
+        # "Referer": "https://www.google.com/",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Accept-Language": "en-US,en;q=0.9"
+    }
+]
+# # Create ordered dict from Headers above
+# ordered_headers_list = []
+# for headers in headers_list:
+#     h = OrderedDict()
+#     for header,value in headers.items():
+#         h[header]=value
+#     ordered_headers_list.append(h)
+    
+    
+url = 'https://httpbin.org/headers'
+
+for i in range(1,4):
+    #Pick a random browser headers
+    headers = random.choice(headers_list)
+    #Create a request session
+    r = requests.Session()
+    r.headers = headers
+    
+    response = r.get(url)
+    print("Request #%d\nUser-Agent Sent:%s\n\nHeaders Recevied by HTTPBin:"%(i,headers['User-Agent']))
+    print(response.json())
+    print("-------------------")
